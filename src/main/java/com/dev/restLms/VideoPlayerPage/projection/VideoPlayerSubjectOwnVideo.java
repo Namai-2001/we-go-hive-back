4696cb743f43c77eb4111473ccde8138c3c12f6b
Namai-2001 <ghmcvbte@naver.com>
2024-12-14 19:00:36 +0900

    정환형꺼 넣기

---
+++ src/main/java/com/dev/restLms/VideoPlayerPage/projection/VideoPlayerSubjectOwnVideo.java
@@ -0,0 +1,17 @@
+package com.dev.restLms.VideoPlayerPage.projection;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class VideoPlayerSubjectOwnVideo {
+  private String episodeId;
+  private String sovOfferedSubjectsId;
+  private String videoSortIndex;
+  private String sovVideoId;
+}
