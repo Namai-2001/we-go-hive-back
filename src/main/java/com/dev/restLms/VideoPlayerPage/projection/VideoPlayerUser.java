704796df3a12d700ec3b679af4aba82736770ae9
Namai-2001 <ghmcvbte@naver.com>
2024-12-15 02:30:55 +0900

    정환형꺼 수정 완료
    검증도 했음

--- src/main/java/com/dev/restLms/VideoPlayerPage/projection/VideoPlayerUser.java
+++ src/main/java/com/dev/restLms/VideoPlayerPage/projection/VideoPlayerUser.java
@@ -1,15 +1,7 @@
 package com.dev.restLms.VideoPlayerPage.projection;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-public class VideoPlayerUser {
-    private String sessionId;
-    private String userName;
+public interface VideoPlayerUser {
+     String getSessionId();
+     String getUserName();
 }
