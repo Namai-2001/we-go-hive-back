93c76f2e66d6725ca0864bf54d344a536fcb3ac0
Namai-2001 <ghmcvbte@naver.com>
2024-12-22 17:53:36 +0900

    중간 저장

---
+++ src/main/java/com/dev/restLms/juhwi/MessageService/dto/MSG_MessagePost_DTO.java
@@ -0,0 +1,9 @@
+package com.dev.restLms.juhwi.MessageService.dto;
+
+import lombok.Data;
+
+@Data
+public class MSG_MessagePost_DTO {
+    private String messageTitle;
+    private String messageContent;
+}
