e1ef1b39e90a23a5a824e0ee7ad04a82c72729c5
Namai-2001 <ghmcvbte@naver.com>
2024-12-23 01:32:56 +0900

    추가 필요 데이터 프로젝션 추가

--- src/main/java/com/dev/restLms/juhwi/MessageService/projection/MSG_GetFriendList_Projection.java
+++ src/main/java/com/dev/restLms/juhwi/MessageService/projection/MSG_GetFriendList_Projection.java
@@ -1,6 +1,7 @@
 package com.dev.restLms.juhwi.MessageService.projection;
 
 public interface MSG_GetFriendList_Projection {
+    String getSessionId();
     String getUserEmail();
     String getUserName();
     String getUserBirth();
