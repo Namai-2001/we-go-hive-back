ad7b439ee10494994c40222534f0d27faba5b0a5
l-sc00 <qwer002497@naver.com>
2024-12-22 17:16:34 +0900

    1차

---
+++ src/main/java/com/dev/restLms/sechan/SurveyMain/projection/SM_C_Projection.java
@@ -0,0 +1,7 @@
+package com.dev.restLms.sechan.SurveyMain.projection;
+
+public interface SM_C_Projection {
+    String getCourseId();
+    String getCourseTitle();
+    String getCourseEndDate();
+}
